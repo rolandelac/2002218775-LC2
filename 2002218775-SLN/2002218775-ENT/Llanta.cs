1b4c488df72745646e7d0fa79ecd2645b3722798
rolandelac <zirex_17@hotmail.com>
2017-05-12 15:28:28 -0500

    primer commit

---
+++ 2002218775-SLN/2002218775-ENT/Llanta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2002218775_ENT
+{
+    public class Llanta
+    {
+    }
+}
